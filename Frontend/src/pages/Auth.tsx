c2c62426d97a8f600a22d3bd65666d934c022618
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-06-02 11:48:05 +0530

    integrate Clerk authentication UI with SignedIn/Out and UserButton

---
+++ Frontend/src/pages/Auth.tsx
@@ -0,0 +1,24 @@
+
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/clerk-react';
+
+import AuthHandler from '../components/Auth';
+
+export default function Auth() {
+
+  const data = useUser()
+  
+
+console.log(data)
+  return (
+    <header>
+      
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn >
+        <UserButton />
+      </SignedIn>
+      <AuthHandler></AuthHandler>
+    </header>
+  );
+}
