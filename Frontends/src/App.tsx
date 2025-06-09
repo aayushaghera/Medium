73913a120066ce6f4e387d2a4b419e4be47e6d00
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-06-09 18:17:30 +0530

    update initial project setup, dependencies, and global styles

--- Frontends/src/App.tsx
+++ Frontends/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+// import Auth from './pages/Auth'
+import Index from './pages/index'
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Write from "./pages/Write";
+import About from "./pages/About";
+import BlogPost from './pages/BlogPost';
+import { BrowserRouter , Route, Routes} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-red-500 font-bold'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+              
+              <Route path="/" element={<Index></Index>} />
+              {/* <Route path="/" element={<Auth></Auth>}/> */}
+              <Route path="/blogs" element={<Blogs />} />
+               <Route path="/contact" element={<Contact />} />
+               <Route path="/write" element={<Write />} />
+               <Route path="/about" element={<About />} />
+               <Route path="/blog/:id" element={<BlogPost />} />
+              {/* <Route path="/signin" element={<Signin />}/>
+              <Route path="/blog/:id" element={<Blog />}/>
+              <Route path="/blogs" element={<Blogs />}/>
+              <Route path="/publish" element={<Publish />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
